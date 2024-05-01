15bc3f1d945b0e699c6b5794b9a1218ff5e079cb
Adao-Angelo <adaobegginer@gmail.com>
2024-05-02 00:26:07 +0100

    add functionality of the error handling

---
+++ src/routers/index_router.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/gemini", (req, res) => {});
+
+export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,27 @@
-import Express from "express";
+import Express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import { router } from "./routers/index_router";
+import { AppError } from "./errors/appErros";
 
 const app = Express();
+
 app.use(Express.json());
+app.use(router);
+
+//error_handling_config
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server Error - ${err.message}`,
+    });
+  }
+);
 
 app.listen(9000, () => console.log("Server listening"));
