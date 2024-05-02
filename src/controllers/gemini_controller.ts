9d2bbf0b9d06ddd9e3845a040a7374e4ca6eb1a7
Adao-Angelo <adaobegginer@gmail.com>
2024-05-02 01:04:15 +0100

    create connection with gemini

--- src/controllers/gemini_controller.ts
+++ src/controllers/gemini_controller.ts
@@ -2,13 +2,26 @@ import { Request, Response } from "express";
 import dotenv from "dotenv";
 import { generatePrompt } from "../utils/generatePrompt";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+
 dotenv.config();
 const google_api_key: string = String(process.env.googleKey);
+const generative_ai = new GoogleGenerativeAI(google_api_key);
 
 class Gemini_Controller {
   constructor() {}
   public getGemini = async (request: Request, response: Response) => {
     const prompt: string = generatePrompt(request.body.prompt);
+    const model = generative_ai.getGenerativeModel({
+      model: "gemini-pro",
+    });
+
+    const result = await model.generateContent(prompt);
+    const message = result.response;
+    const text = message.text();
+    return response.status(200).json({
+      status: "success",
+      message: text,
+    });
   };
 }
 
