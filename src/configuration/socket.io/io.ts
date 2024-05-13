73c585ecd2f4b26519b87f01c228d3ffa3bb20ee
Adao-Angelo <adaobegginer@gmail.com>
2024-05-13 20:59:44 +0100

    create io

--- src/configuration/socket.io/io.ts
+++ src/configuration/socket.io/io.ts
@@ -5,8 +5,9 @@ io.on("connection", (socket) => {
   socket.join(socket.id);
 
   socket.on("message", async (message) => {
-    const result = await gemini_controller.getGemini(message.text);
-    socket.emit("gemini", result);
+    // const result = await gemini_controller.getGemini(message.text);
+    console.log(message);
+    socket.emit("response", message);
   });
 
   socket.on("send-audio", (data) => {
---
+++ src/routers/accounts_router.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const accounts_router = Router();
+
+accounts_router.post("/", (request, response) => {});
+
+export { accounts_router };
